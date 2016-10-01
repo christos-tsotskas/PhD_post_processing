9b679ee363c87cd72d4a02bd85d3402d9d2a53c8
c.tsotskas <c.tsotskas@gmail.com>
2016-10-01 16:21:31 +0100

    streamlined the generation and added material for ZDT1

--- src/final_post_processing.py
+++ src/final_post_processing.py
@@ -4,6 +4,12 @@ Plot
 Created on 1 Oct 2016
 
 @author: DefaultUser
+
+notes for Ipython:
+    In [1]: %load_ext autoreload
+    In [2]: %autoreload 2
+    In [3]: from foo import some_function
+    In [4]: some_function()
 '''
 
 from pylab import scatter
@@ -14,7 +20,11 @@ from pandas import Series, DataFrame
 import numpy as np
 
 
-def process_file(filename, variables_column_name, marker, label, selected_column):
+def process_file(case_specifications , selected_column):
+    filename = case_specifications['filename']
+    variables_column_name = case_specifications['variables_column_name']
+    marker = case_specifications['mark']
+    label = case_specifications["description"]
     data1 = DataFrame(pd.read_csv(filename, delim_whitespace=True))
     
     gb1 = data1.groupby([variables_column_name]).mean()
@@ -22,7 +32,7 @@ def process_file(filename, variables_column_name, marker, label, selected_column
     y1 = gb1[selected_column]
     plt.scatter(x, y1, s=12, marker=marker, label = label)
 
-def plot_all_for(type = 'time'):
+def plot_all_for(type = 'time', test_function_name="ZDT2"):
     '''
     type: 'time' = time(s):
     type: 'HV' = HyperVolume
@@ -40,27 +50,26 @@ def plot_all_for(type = 'time'):
                     
                     }
     
-    f1 = 'all_tests_phd_corrections4_20000evals.txt'
-    f2 = 'all_tests_phd_corrections5_20000evals.txt'
-    f3 = 'all_tests_phd_corrections7_20000evals.txt'
-    f4 = 'all_tests_phd_corrections6_20000evals.txt'
-    f5 = 'all_tests_phd_corrections8_20000evals.txt'
+    f1 = {'filename':'all_tests_phd_corrections4_20000evals.txt', 'description':'nVar/5', 'mark':'o', 'variables_column_name':"#number_of_variables"}
+    f2 = {'filename':'all_tests_phd_corrections5_20000evals.txt', 'description':'nVar/5', 'mark':'s', 'variables_column_name':"#nVar"}
+    f3 = {'filename':'all_tests_phd_corrections7_20000evals.txt', 'description':'nVar/9', 'mark':'*', 'variables_column_name':"#nVar"}
+    f4 = {'filename':'all_tests_phd_corrections6_20000evals.txt', 'description':'nVar/11', 'mark':'+', 'variables_column_name':"#nVar"}
+    f5 = {'filename':'all_tests_phd_corrections8_20000evals.txt', 'description':'nVar/13', 'mark':'.', 'variables_column_name':"#nVar"}
+    
+    filenames =[f1, f2, f3, f4, f5]
     
     fig = plt.figure()
     
-    process_file(f1, '#number_of_variables', "o", "case4_", type_of_plot[type]['column_name'])
-    process_file(f2, '#nVar', "s", "case5_", type_of_plot[type]['column_name'])
-    process_file(f3, '#nVar', "*", "case7_", type_of_plot[type]['column_name'])
-    process_file(f4, '#nVar', "+", "case6_", type_of_plot[type]['column_name'])
-    process_file(f5, '#nVar', ".", "case8_", type_of_plot[type]['column_name'])
-    
+    for filename in filenames:
+        process_file(filename, type_of_plot[type]['column_name'])
+
     plt.xlabel('Number of variables')
     plt.ylabel(type_of_plot[type]['ylabel'])
     plt.gca().set_xscale('log')
-    plt.title(type_of_plot[type]['title'])
+    plt.title(type_of_plot[type]['title']+" for "+test_function_name)
     plt.grid(True)
     plt.legend(loc='upper left')
     plt.show()
     
-# plot_all_for(type='time')
-plot_all_for(type='HV')
+plot_all_for(type='time', test_function_name="ZDT2")
+# plot_all_for(type='HV')
