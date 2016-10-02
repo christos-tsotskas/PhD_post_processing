16d86c448c2a49d352fba672f2aa818db7542b8b
c.tsotskas <c.tsotskas@gmail.com>
2016-10-02 23:22:03 +0100

    fixed the axis' range for each type of plot at the same scale (for comparisions)

--- src/final_post_processing.py
+++ src/final_post_processing.py
@@ -38,14 +38,18 @@ def plot_all_for(specifications_of_cases_to_compare, type = 'time', test_functio
     type: 'HV' = HyperVolume
     '''
     type_of_plot = {'time':{
-                            'ylabel':'Time(s)',
-                            'title':'Variables - Time Scalability',
-                            'column_name':'time(s):'
+                            'ylabel':'Elapsed Time(s)',
+                            'title':'Variables - Elapsed Time Scalability',
+                            'column_name':'time(s):',
+                            'y_axis_range_min':-50,
+                            'y_axis_range_max':400                            
                             },
                     'HV':{
                           'ylabel':'Hypervolume',
                           'title':'Variables - Hypervolume Scalability',
-                          'column_name':'HV'
+                          'column_name':'HV',
+                          'y_axis_range_min':0,
+                          'y_axis_range_max':6000,                          
                           }
                     
                     }
@@ -63,6 +67,8 @@ def plot_all_for(specifications_of_cases_to_compare, type = 'time', test_functio
     plt.title(type_of_plot[type]['title']+" for "+test_function_name)
     plt.grid(True)
     plt.legend(loc='upper left')
+    
+    plt.ylim(type_of_plot[type]['y_axis_range_min'], type_of_plot[type]['y_axis_range_max'])
     plt.show()
     
 def plot_for_ZDT2():
@@ -78,14 +84,14 @@ def plot_for_ZDT2():
     
 def plot_for_ZDT1():
 #     f1 = {'filename':'all_tests_phd_corrections10b_zdt1_20000evals.txt', 'description':'nVar/13a', 'mark':'o', 'variables_column_name':"#nVar"}
-    f2 = {'filename':'all_tests_phd_corrections10e_zdt1_20000evals.txt', 'description':'nVar/13', 'mark':'s', 'variables_column_name':"#nVar"}
-    f3 = {'filename':'all_tests_phd_corrections11_zdt1_20000evals.txt', 'description':'nVar/11', 'mark':'*', 'variables_column_name':"#nVar"}
-    f4 = {'filename':'all_tests_phd_corrections12_zdt1_20000evals.txt', 'description':'nVar/9', 'mark':'+', 'variables_column_name':"#nVar"}
-    f5 = {'filename':'all_tests_phd_corrections13_zdt1_20000evals.txt', 'description':'nVar/5', 'mark':'.', 'variables_column_name':"#nVar"}
+    f5 = {'filename':'all_tests_phd_corrections13_zdt1_20000evals.txt', 'description':'nVar/5', 'mark':'s', 'variables_column_name':"#nVar"}
+    f4 = {'filename':'all_tests_phd_corrections12_zdt1_20000evals.txt', 'description':'nVar/9', 'mark':'*', 'variables_column_name':"#nVar"}
+    f3 = {'filename':'all_tests_phd_corrections11_zdt1_20000evals.txt', 'description':'nVar/11', 'mark':'+', 'variables_column_name':"#nVar"}
+    f2 = {'filename':'all_tests_phd_corrections10e_zdt1_20000evals.txt', 'description':'nVar/13', 'mark':'.', 'variables_column_name':"#nVar"}
 
-    specifications_of_cases_to_compare_for_ZDT1 =[f2, f3, f4, f5]
+    specifications_of_cases_to_compare_for_ZDT1 =[f5, f4, f3, f2]
     plot_all_for(type='time', test_function_name="ZDT1", specifications_of_cases_to_compare=specifications_of_cases_to_compare_for_ZDT1)
     plot_all_for(type='HV', test_function_name="ZDT1", specifications_of_cases_to_compare=specifications_of_cases_to_compare_for_ZDT1)
 
-plot_for_ZDT1()
+plot_for_ZDT2()
     
